6785174e4d13a420aec126ccc338a24bfa308409
anastasyawlf <najya.anastasya275@gmail.com>
2024-04-30 17:17:40 +0700

    Initial commit

---
+++ fizzbuzz.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// Nilai maksimum bernilai n
+	fmt.Print("Type a number: ")
+	var n int
+	fmt.Scanf("%d", &n)
+
+	// Proses nilai n berdasarkan fungsi fizzBuzz
+	for i := 1; i <= n; i++ {
+		fizzBuzz(i)
+	}
+
+}
+
+func fizzBuzz(i int) {
+
+	if i%3 == 0 && i%5 == 0 {
+		fmt.Println("FizzBuzz")
+	} else if i%3 == 0 {
+		fmt.Println("Fizz")
+	} else if i%5 == 0 {
+		fmt.Println("Buzz")
+	} else {
+		fmt.Println(i)
+	}
+
+}
