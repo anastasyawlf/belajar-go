955be9916fb0adbb4486d13563e45208626540a7
anastasyawlf <najya.anastasya275@gmail.com>
2024-12-12 22:40:25 +0700

    darinolyakak

---
+++ pointer/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	giver, _ := strconv.Atoi(scanner.Text())
+	scanner.Scan()
+	receiver, _ := strconv.Atoi(scanner.Text())
+	scanner.Scan()
+	marble, _ := strconv.Atoi(scanner.Text())
+
+	giveMarble(&giver, &receiver, marble)
+	fmt.Printf("%d \n", giver)
+	fmt.Printf("%d", receiver)
+}
+
+//Tulis kode disini
+
+func giveMarble(giver *int, receiver *int, marble int) (int, int) {
+	if marble < *giver {
+		*giver -= marble
+		*receiver += marble
+	} else {
+		fmt.Println("Kelereng tidak cukup untuk dibagikan")
+	}
+	return *giver, *receiver
+}
